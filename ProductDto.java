caf357aee70625ef44229a96c6ec1c071fd50ae8
JohnPhilip80 <rushtophilip@gmail.com>
2025-04-09 13:44:35 +0530

    Create ProductDto.java

---
+++ ProductDto.java
@@ -0,0 +1,41 @@
+package com.john.mystore.models;
+
+public class ProductDto {
+	
+	private int id;
+	private String name;
+	private String brand;
+	private String category;
+	private double price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}

af51f5d0e2b9972868ac5988d013cd4adbd7359b
JohnPhilip80 <rushtophilip@gmail.com>
2025-04-09 13:43:53 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,68 @@
+package com.john.mystore.models;
+
+import java.util.Date;
+
+public class Product {
+	private int id;
+	private String name;
+	private String brand;
+	private String category;
+	private double price;
+	private Date createdAt;
+	private String createdBy;
+	public Product() {
+		super();
+	}
+	public Product(int id, String name, String brand, String category, double price, Date createdAt, String createdBy) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.category = category;
+		this.price = price;
+		this.createdAt = createdAt;
+		this.createdBy = createdBy;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+}
